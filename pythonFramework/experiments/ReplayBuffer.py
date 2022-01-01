25d49e7066794692084576faff33fb86da7d33af
Ambrozy <bushmanov.vova@yandex.ru>
2022-01-01 22:19:11 +0500

    fix replay buffer

--- pythonFramework/experiments/ReplayBuffer.py
+++ pythonFramework/experiments/ReplayBuffer.py
@@ -7,27 +7,27 @@ class ReplayBuffer:
         self.episodeLength = length
         self.batchSize = batchSize
         self.leftEpisodesAfterOverflow = leftEpisodesAfterOverflow or length
-        self.__data = list()
+        self._data = list()
 
     def count(self):
         return {
-            'episodes': len(self.__data),
+            'episodes': len(self._data),
             'steps': self.length,
         }
 
-    def __updateLength(self):
-        self.length = sum(map(lambda episode: len(episode), self.__data))
+    def _updateLength(self):
+        self.length = sum(map(lambda episode: len(episode), self._data))
 
     def push(self, episode):
         if len(episode) > 0:
-            self.__data.append(episode)
-            self.__data.sort(key=lambda x: getFinalScore(x, -2))
+            self._data.append(episode)
+            self._data.sort(key=lambda x: getFinalScore(x, -2))
             self.length += len(episode)
 
-        if len(self.__data) > self.episodeLength:
+        if len(self._data) > self.episodeLength:
             sliceLength = self.leftEpisodesAfterOverflow or self.episodeLength
-            self.__data = self.__data[0:sliceLength]
-            self.__updateLength()
+            self._data = self._data[0:sliceLength]
+            self._updateLength()
 
     def getDataset(self):
         raise Exception('getDataset is not implemented')
